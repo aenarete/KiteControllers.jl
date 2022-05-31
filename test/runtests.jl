08ed71666e233f1ab2eaafae5afae4f3aae78773
Uwe Fechner <uwe.fechner.msc@gmail.com>
2022-05-31 19:29:38 +0200

    More tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -99,3 +99,13 @@ end
     end
     @test out == [0,0,0.5,1,1.5,2,2.5,3,3,2.5,2,1.5,1,0.5,0,0]
 end
+
+@testset "Mixer_2CH" begin
+    m2 = Mixer_2CH(1.0)
+    x=[0,1,2,3,4,3,2,1,0,1,2]
+    y=-x
+    for i in 1:length(x)
+        in1=x[i]
+        in2=y[i]
+    end
+end
