ad7446b392b9459becf291977f5a03cb87407d67
Uwe Fechner <uwe.fechner.msc@gmail.com>
2022-05-31 19:53:11 +0200

    Fix tests

--- src/KiteControllers.jl
+++ src/KiteControllers.jl
@@ -4,7 +4,7 @@ using KiteUtils, Parameters, StaticArrays, NLsolve, Printf
 import Base.reset
 
 export Integrator, FlightPathController, FPCSettings, WCSettings    # types
-export UnitDelay, RateLimiter
+export UnitDelay, RateLimiter, Mixer_2CH
 export saturate, wrap2pi                                            # utility function
 export reset, update, on_timer                                      # functions on Integrator
 export on_control_command, on_est_sysstate, on_timer, calc_steering # functions on FlightPathController 
