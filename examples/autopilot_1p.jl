ec7e8d8971d4d3158bf7668df79301a3fa594bae
Uwe Fechner <fechner@aenarete.eu>
2022-10-30 21:42:39 +0100

    Add autopilot_1p example

---
+++ examples/autopilot_1p.jl
@@ -0,0 +1,125 @@
+# activate the test environment if needed
+using Pkg
+if ! ("Plots" ∈ keys(Pkg.project().dependencies))
+    using TestEnv; TestEnv.activate()
+end
+using Timers; tic()
+
+using KiteControllers, KiteViewers, KiteModels
+
+# change this to KPS3 or KPS4
+const Model = KPS3
+
+if ! @isdefined kcu;    const kcu = KCU(se());   end
+if ! @isdefined kps4;   const kps4 = Model(kcu); end
+
+wcs = WCSettings(); update(wcs); wcs.dt = 1/se().sample_freq
+const fcs = FPCSettings(); fcs.dt = wcs.dt
+const fpps = FPPSettings()
+const ssc = SystemStateControl(wcs, fcs, fpps)
+dt = wcs.dt
+
+# the following values can be changed to match your interest
+if ! @isdefined MAX_TIME; MAX_TIME=3600; end
+TIME_LAPSE_RATIO = 2
+SHOW_KITE = true
+# end of user parameter section #
+
+phi_set = 21.48
+# on_control_command(ssc.fpp.fpca.fpc, attractor=[deg2rad(phi_set), deg2rad(51.88)])
+# on_control_command(ssc.fpp.fpca.fpc, psi_dot_set=-23.763, radius=-4.35)
+
+if ! @isdefined viewer; const viewer = Viewer3D(SHOW_KITE); end
+
+steps = 0
+
+function simulate(integrator)
+    start_time_ns = time_ns()
+    clear_viewer(viewer)
+    i=1
+    j=0; k=0
+    GC.gc()
+    max_time = 0
+    t_gc_tot = 0
+    sys_state = SysState(kps4)
+    on_new_systate(ssc, sys_state)
+    while true
+        if i > 100
+            dp = KiteControllers.get_depower(ssc)
+            if dp < 0.22 dp = 0.22 end
+            steering = calc_steering(ssc)
+            set_depower_steering(kps4.kcu, dp, steering)
+        end
+        if i == 200
+            on_autopilot(ssc)
+        end
+        # execute winch controller
+        v_ro = calc_v_set(ssc)
+        #
+        t_sim = @elapsed KiteModels.next_step!(kps4, integrator, v_ro=v_ro, dt=dt)
+        if t_sim < 0.3*dt
+            t_gc_tot += @elapsed GC.gc(false)
+        end
+        sys_state = SysState(kps4)
+        on_new_systate(ssc, sys_state)
+        if mod(i, TIME_LAPSE_RATIO) == 0 
+            KiteViewers.update_system(viewer, sys_state; scale = 0.04/1.1, kite_scale=6.6)
+            set_status(viewer, String(Symbol(ssc.state)))
+            wait_until(start_time_ns + 1e9*dt, always_sleep=true) 
+            mtime = 0
+            if i > 10/dt 
+                # if we missed the deadline by more than 5 ms
+                mtime = time_ns() - start_time_ns
+                if mtime > dt*1e9 + 5e6
+                    print(".")
+                    j += 1
+                end
+                k +=1
+            end
+            if mtime > max_time
+                max_time = mtime
+            end            
+            start_time_ns = time_ns()
+            t_gc_tot = 0
+        end
+        if viewer.stop break end
+        if i*dt > MAX_TIME break end
+        i += 1
+    end
+    misses = j/k * 100
+    println("\nMissed the deadline for $(round(misses, digits=2)) %. Max time: $(round((max_time*1e-6), digits=1)) ms")
+    return div(i, TIME_LAPSE_RATIO)
+end
+
+function play()
+    global steps
+    integrator = KiteModels.init_sim!(kps4, stiffness_factor=0.04)
+    toc()
+    steps = simulate(integrator)
+    GC.enable(true)
+end
+
+function async_play()
+    if viewer.stop
+        @async begin
+            play()
+            stop(viewer)
+        end
+    end
+end
+
+function parking()
+    on_parking(ssc)
+end
+
+function autopilot()
+    on_autopilot(ssc)
+end
+
+on(viewer.btn_PLAY.clicks) do c; async_play(); end
+on(viewer.btn_STOP.clicks) do c; stop(viewer); on_stop(ssc) end
+on(viewer.btn_PARKING.clicks) do c; parking(); end
+on(viewer.btn_AUTO.clicks) do c; autopilot(); end
+
+play()
+stop(viewer)
