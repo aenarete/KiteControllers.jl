55d545e7029afadfca0ffd85a3b6f5b7eee0a1cc
Uwe Fechner <fechner@aenarete.eu>
2023-03-04 19:58:31 +0100

    Update dependencies

--- test/create_sys_image.jl
+++ test/create_sys_image.jl
@@ -6,14 +6,14 @@ if ! ("Plots" ∈ keys(Pkg.project().dependencies))
 end
 
 @info "Loading packages ..."
-using BenchmarkTools, Plots, KiteUtils, NLsolve, Parameters, StaticArrays, WinchModels, PackageCompiler, KiteViewers, 
+using BenchmarkTools, Plots, KiteUtils, NLsolve, Parameters, StaticArrays, StaticArrayInterface, WinchModels, PackageCompiler, KiteViewers, 
       KiteModels, KitePodModels, StructTypes, YAML
 
 @info "Creating sysimage ..."
 push!(LOAD_PATH,joinpath(pwd(),"src"))
 
 PackageCompiler.create_sysimage(
-    [:BenchmarkTools, :Plots, :KiteUtils, :NLsolve, :Parameters, :StaticArrays, :WinchModels, :KiteViewers, 
+    [:BenchmarkTools, :Plots, :KiteUtils, :NLsolve, :Parameters, :StaticArrays, :StaticArrayInterface, :WinchModels, :KiteViewers, 
      :KiteModels, :KitePodModels, :StructTypes, :YAML];
     sysimage_path="kps-image_tmp.so",
     precompile_execution_file=joinpath("test", "test_for_precompile.jl")
