e593a5d76fd7a474ea9369dfe05df5604f9952e9
Uwe Fechner <uwe.fechner.msc@gmail.com>
2023-03-07 17:23:22 +0100

    Improving tuning

--- examples/tune_1p.jl
+++ examples/tune_1p.jl
@@ -24,7 +24,7 @@ TIME_LAPSE_RATIO = 1
 SHOW_KITE = false
 # end of user parameter section #
 
-steps = 0
+LAST_RES = 1e10
 if ! @isdefined T;       const T = zeros(Int64(MAX_TIME/dt)); end
 if ! @isdefined AZIMUTH; const AZIMUTH = zeros(Int64(MAX_TIME/dt)); end
 
@@ -60,14 +60,24 @@ end
 
 # tests the parking controller and returns the sum of the square of 
 # the azimuth error in degrees squared and divided by the test duration
-function test_parking()
+function test_parking(suppress_overshoot_factor = 6.0)
+    global LAST_RES
     clear!(kps)
     init_kcu(kcu, se())
     AZIMUTH .= zeros(Int64(MAX_TIME/dt))
     integrator = KiteModels.init_sim!(kps, stiffness_factor=0.04)
     simulate(integrator)
-    res=sum(abs2.(rad2deg.(AZIMUTH)))/40
-    if res < 200
+    res = sum(abs2.(rad2deg.(AZIMUTH)))/40
+    n = length(AZIMUTH)
+    overshoot = zeros(n)
+    for i in 1:n
+        if AZIMUTH[i] < 0
+            overshoot[i] = -AZIMUTH[1]
+        end
+    end
+    res += sum(abs2.(rad2deg.(overshoot)))/40 * suppress_overshoot_factor
+    if res < LAST_RES
+        LAST_RES = res
         println(res, " p: ", fcs.p, " d: ", fcs.d)
         display(show_result())
     end
@@ -85,16 +95,32 @@ function f(x)
     test_parking()
 end
 
-function tune_1p()
+function tune_1p(robustness_factor=0.95)
     config = ConfigParameters()         # calls initialize_parameters_to_default of the C API
-    config.noise=1e-2
-    # config.n_inner_iterations=UInt(95)
+    config.noise = 1e-6
+    config.n_iterations = 60
     println(config.noise)
     println(config.n_inner_iterations)
-    # set_kernel!(config, "kMaternARD5")  # calls set_kernel of the C API
-    # config.sc_type = SC_MAP
-    lowerbound = [10., 0., 10.]; upperbound = [30., 1.0, 40.]
-    optimizer, optimum = bayes_optimization(f, lowerbound, upperbound, config)
+    set_kernel!(config, "kMaternARD5")  # calls set_kernel of the C API
+    config.sc_type = SC_MAP
+    lowerbound = [10., 0., 10.]; upperbound = [30., 1.0, 50.]
+    x, optimum = bayes_optimization(f, lowerbound, upperbound, config)
+    fcs.p = x[1]*robustness_factor
+    fcs.i = x[2]*robustness_factor
+    fcs.d = x[3]*robustness_factor
+    test_parking()
+    show_result()
+end
+
+function est_noise(n=10)
+    res = zeros(n)
+    for i in 1:n
+        res[i] = test_parking()
+    end
+    # println(res)
+    avg = sum(res)/n
+    noise = res .- avg
+    rel_noise = sum(abs2.(noise) / abs2(avg))/n
 end
 
 fcs.p=13.63*0.9
