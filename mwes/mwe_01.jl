f45987b019a84b025862459d04eead8150781c0b
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-03-25 11:37:16 +0100

    simplify mwe

--- mwes/mwe_01.jl
+++ mwes/mwe_01.jl
@@ -2,13 +2,11 @@ using KiteViewers.GLMakie, NativeFileDialog
 
 const running    = Observable(false)
 fig = Figure(size=(200, 200), backgroundcolor=RGBf(0.7, 0.8, 1))
-sub_fig = fig[1,1]
-fig[2, 1] = buttongrid = GridLayout(tellwidth=false)
-btn_RUN  = Button(sub_fig, label = " RUN ")
-buttongrid[1, 1:1] = [btn_RUN]
+btn_RUN  = Button(fig, label = " Open file... ")
 on(btn_RUN.clicks) do c;
     println("opening file dialog...")
-    filename      = pick_file(pwd())
+    # @threadcall pick_file()
+    filename      = pick_file("")
     println(filename)
 end
 fig
