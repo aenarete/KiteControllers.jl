60bd04434c2bf577a92711aba9f5da8d7809ecf4
Uwe Fechner <fechner@aenarete.eu>
2024-03-28 11:05:00 +0100

    nice message

--- mwes/mwe_03.jl
+++ mwes/mwe_03.jl
@@ -3,30 +3,32 @@ using Gtk4, Printf
 
 struct Stats
     energy::Float64
+    max_force::Float64
+    min_height::Float64
 end
 
-stats=Stats(333)
+stats=Stats(333, 3900, 25)
 
 function show_stats(stats::Stats)
-    win = GtkWindow("My First Gtk4.jl Program", 400, 200)
+    win = GtkWindow("Statistics", 400, 200)
     css = """
-    button {
-        font-size: 3.0em;
+    label {
+        font-size: 2.0em;
     }
     """
     cssProvider = GtkCssProvider(css)
     push!(Gtk4.display(win), cssProvider)
 
-    hbox = GtkBox(:h)  # :h makes a horizontal layout, :v a vertical layout
-    push!(win, hbox)
+    vbox = GtkBox(:v, homogeneous=true)
 
-    label = GtkButton("  Power:")
-    value = GtkButton("  22.4")
-    push!(hbox, label)
-    push!(hbox, value)
-    hbox.spacing = 10
-    hbox.homogeneous = true
+    hbox = GtkBox(:h, homogeneous=true)  # :h makes a horizontal layout, :v a vertical layout
+    label = GtkLabel("Power:"); val = GtkLabel(@sprintf("%5.0f Wh", stats.energy))
+    push!(hbox, label, val)
+
+    push!(vbox, hbox)
+    push!(win, vbox)
     show(win)
     nothing
 end
+
 
