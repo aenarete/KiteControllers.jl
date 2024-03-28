7b2497e4e4bfdb9b2a448f9d005c33924b41d9b2
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-03-28 15:06:50 +0100

    Update mwe_04

--- mwes/mwe_04.jl
+++ mwes/mwe_04.jl
@@ -16,10 +16,24 @@ stats = Stats(333, 3900, 25)
 function show_stats(stats::Stats)
     fig = GLMakie.Figure(size = (400, 400))
     lscene = GLMakie.LScene(fig[1, 1], show_axis=false)
-    lbl   = "energy:"
-    value = @sprintf("%5.0f Wh", stats.e_mech)
-    GLMakie.text!(lscene, position =  GLMakie.Point2f( 10, 340), lbl, fontsize = 24, space=:pixel)
-    GLMakie.text!(lscene, position =  GLMakie.Point2f(250, 340), value, fontsize = 24, space=:pixel)
+    if Sys.islinux()
+        lin_font="/usr/share/fonts/truetype/ttf-bitstream-vera/VeraMono.ttf"
+        if isfile(lin_font)
+            font=lin_font
+        else
+            font="/usr/share/fonts/truetype/freefont/FreeMono.ttf"
+        end
+    else
+        font="Courier New"
+    end
+    function print(lbl::String, value::String; line, font=font)
+        GLMakie.text!(lscene, position =  GLMakie.Point2f( 10, 360-line*32), lbl; fontsize = 24, space=:pixel)
+        GLMakie.text!(lscene, position =  GLMakie.Point2f(250, 360-line*32), value; fontsize = 24, font, space=:pixel)
+        line +=1    
+    end
+    line = print("energy:    ", @sprintf("%5.0f Wh", stats.e_mech); line = 1)
+    line = print("max force: ", @sprintf("%5.0f  N", stats.max_force); line)
+    line = print("min height:", @sprintf("%5.0f  m", stats.min_height); line)
     # @sprintf("%5.0f  N", stats.max_force)
     # @sprintf("%5.0f  m", stats.min_height)
 
