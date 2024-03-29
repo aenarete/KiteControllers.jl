ce66fe3d62a7c7717c3adfe1f790b8ad62d98ecb
Uwe Fechner <fechner@aenarete.eu>
2024-03-29 12:02:06 +0100

    add plot_side_view

--- examples/autopilot.jl
+++ examples/autopilot.jl
@@ -36,7 +36,7 @@ SHOW_KITE         = true
 # end of user parameter section #
 
 viewer::Viewer3D = Viewer3D(SHOW_KITE)
-viewer.menu.options[]=["plot_main", "plot_power", "plot_control", "plot_elev_az", "plot_timing", "print_stats", "load logfile", "save logfile"]
+viewer.menu.options[]=["plot_main", "plot_power", "plot_control", "plot_elev_az", "plot_side_view", "plot_timing", "print_stats", "load logfile", "save logfile"]
 viewer.menu_rel_tol.options[]=["0.0005","0.0001","0.00005", "0.00001","0.000005","0.000001"]
 viewer.menu_rel_tol.i_selected[]=1
 PARKING::Bool = false
@@ -227,69 +227,9 @@ function save_log_as()
 end
 
 include("logging.jl")
-
-function plot_timing()
-    log = load_log(PARTICLES, basename(KiteViewers.plot_file[]))
-    sl  = log.syslog
-    display(ControlPlots.plotx(sl.time, sl.t_sim, 100*sl.steering, 100*sl.depower;
-                               ylabels=["t_sim [ms]", "steering [%]","depower [%]"],
-                               fig="timing"))
-    println("Mean    time per timestep: $(mean(sl.t_sim)) ms")
-    println("Maximum time per timestep: $(maximum(sl.t_sim)) ms")
-    index = Int64(round(12/dt))
-    println("Maximum for t>12s        : $(maximum(sl.t_sim[index:end])) ms")
-    plt.pause(0.01)
-    plt.show(block=false)
-    nothing
-end
-
-function plot_main()
-    log = load_log(PARTICLES, basename(KiteViewers.plot_file[]))
-    sl  = log.syslog
-    display(plotx(log.syslog.time, log.z, rad2deg.(sl.elevation), rad2deg.(sl.azimuth), sl.l_tether, sl.force, sl.v_reelout;
-            ylabels=["height [m]", "elevation [°]", "azimuth [°]", "length [m]", "force [N]", "v_ro [m/s]"],
-            fig="main"))
-    plt.pause(0.01)
-    plt.show(block=false)
-    nothing
-end
-
-function plot_power()
-    log = load_log(PARTICLES, basename(KiteViewers.plot_file[]))
-    sl  = log.syslog
-    energy = similar(sl.v_reelout)
-    en=0.0
-    for i in eachindex(energy)
-        en +=  sl.force[i]*sl.v_reelout[i]*dt
-        energy[i] = en
-    end
-    display(plotx(log.syslog.time, sl.force, sl.v_reelout, sl.force.*sl.v_reelout, energy./3600;
-            ylabels=["force [N]", L"v_\mathrm{ro}~[m/s]", L"P_\mathrm{m}~[W]", "Energy [Wh]"],
-            fig="power"))
-    plt.pause(0.01)
-    plt.show(block=false)
-    nothing
-end
-
-function plot_control()
-    println("plot control...")
-    # elevation, azimuth
-    # depower, steering
-    # state
-    log = load_log(PARTICLES, basename(KiteViewers.plot_file[]))
-    sl  = log.syslog
-    display(plotx(log.syslog.time, rad2deg.(sl.elevation), rad2deg.(sl.azimuth), 100*sl.depower, 100*sl.steering, sl.sys_state;
-            ylabels=["elevation [°]", "azimuth [°]", "depower [%]", "steering [%]", "fpp_state"],
-            fig="control"))
-    plt.pause(0.01)
-    plt.show(block=false)
-    nothing
-end
-
-function plot_side_view()
-end
-
+include("plots.jl")
 include("stats.jl")
+
 function print_stats()
     log = load_log(PARTICLES, basename(KiteViewers.plot_file[]))
     sl  = log.syslog
@@ -307,18 +247,6 @@ function print_stats()
     show_stats(stats)
 end
 
-function plot_elev_az()
-    log = load_log(PARTICLES, basename(KiteViewers.plot_file[]))
-    sl  = log.syslog
-    display(plotxy(rad2deg.(sl.azimuth), rad2deg.(sl.elevation);
-            ylabel="elevation [°]",
-            xlabel="azimuth [°]",
-            fig="elev_az"))
-    plt.pause(0.01)
-    plt.show(block=false)
-    nothing
-end
-
 function do_menu(c)
     if c == "save logfile"
         save_log_as()
@@ -334,6 +262,8 @@ function do_menu(c)
         plot_elev_az()
     elseif c == "plot_main"
         plot_main()
+    elseif c == "plot_side_view"
+        plot_side_view()
     elseif c == "print_stats"
         print_stats()
     end
