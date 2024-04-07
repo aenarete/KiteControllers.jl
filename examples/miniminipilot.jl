0cb04f7afd01b3015832a2ea2e0bcd95080e3c9e
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-04-07 19:04:53 +0200

    function sim() works

--- examples/miniminipilot.jl
+++ examples/miniminipilot.jl
@@ -5,69 +5,52 @@
 
 using KiteControllers
 
-kcu::KiteModels.KCU   = KiteModels.KCU(KiteControllers.se())
-kps4::KiteModels.KPS4 = KiteModels.KPS4(kcu)
-
-wcs = WCSettings(); update(wcs); wcs.dt = 1/KiteControllers.se().sample_freq
-fcs::FPCSettings = FPCSettings(); fcs.dt = wcs.dt
-fpps::FPPSettings = FPPSettings()
-ssc::SystemStateControl = SystemStateControl(wcs, fcs, fpps)
-dt::Float64 = wcs.dt
-
-function init_globals()
-    global kcu, kps4, wcs, fcs, fpps, ssc
+# run a simulation using a correction vector, return a log object
+function sim(sim_time=460; corrections=Float64[])
     kcu   = KiteModels.KCU(KiteControllers.se())
     kps4 = KiteModels.KPS4(kcu)
+
     wcs = WCSettings(); update(wcs); wcs.dt = 1/KiteControllers.se().sample_freq
     fcs = FPCSettings(); fcs.dt = wcs.dt
     fpps = FPPSettings()
     ssc = SystemStateControl(wcs, fcs, fpps)
-end
+    dt = wcs.dt
 
-# the following values can be changed to match your interest
-MAX_TIME::Float64 = 460
-# end of user parameter section #
-
-steps = 0
-
-function simulate(integrator)
-    i = 1
-    sys_state = KiteModels.SysState(kps4)
-    on_new_systate(ssc, sys_state)
-    while true
-        if i > 100
-            dp = KiteControllers.get_depower(ssc)
-            if dp < 0.22 dp = 0.22 end
-            steering = calc_steering(ssc)
-            KiteModels.set_depower_steering(kps4.kcu, dp, steering)
-        end
-        if i == 200
-            on_autopilot(ssc)
-        end
-        # execute winch controller
-        v_ro = calc_v_set(ssc)
-        #
-        t_sim = @elapsed KiteModels.next_step!(kps4, integrator, v_ro=v_ro, dt=dt)
+    function simulate(integrator)
+        i = 1
         sys_state = KiteModels.SysState(kps4)
         on_new_systate(ssc, sys_state)
-        i += 1
-        if i*dt > MAX_TIME break end
+        while true
+            if i > 100
+                dp = KiteControllers.get_depower(ssc)
+                if dp < 0.22 dp = 0.22 end
+                steering = calc_steering(ssc)
+                KiteModels.set_depower_steering(kps4.kcu, dp, steering)
+            end
+            if i == 200
+                on_autopilot(ssc)
+            end
+            # execute winch controller
+            v_ro = calc_v_set(ssc)
+            #
+            t_sim = @elapsed KiteModels.next_step!(kps4, integrator, v_ro=v_ro, dt=dt)
+            sys_state = KiteModels.SysState(kps4)
+            on_new_systate(ssc, sys_state)
+            i += 1
+            if i*dt > sim_time
+                break 
+            end
+        end
+        nothing
     end
-    nothing
-end
 
-function play()
-    init_globals()
-    on_parking(ssc)
-    integrator=KiteModels.init_sim!(kps4, stiffness_factor=1.0)
-    simulate(integrator)
-end
+    function play()
+        on_parking(ssc)
+        integrator=KiteModels.init_sim!(kps4, stiffness_factor=0.04)
+        simulate(integrator)
+    end
 
-play()
-println("Stopping...")
-on_stop(ssc)
-KiteModels.clear!(kps4)
-
-# run a simulation using a correction vector, return a log object
-function sim(t_sim=460; corrections=Float64[])
+    play()
+    println("Stopping...")
+    on_stop(ssc)
 end
