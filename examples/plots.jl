62130d5bd443eac233aef67af391bc4df5183b3f
Uwe Fechner <fechner@aenarete.eu>
2024-06-17 17:11:11 +0200

    Add control_plot_II() (#33)
    * Add control_plot_II()
    
    * Add file to .gitignore
    
    * Remove support for Julia 1.9
    
    * add chi (course) to the plot
    
    * blank chi_set during the turns
    
    * add psi_dot_set to plot
    
    * add psi_dot
    
    ---------
    
    Co-authored-by: Uwe Fechner <u.fechner-1@tudelft.nl>

--- examples/plots.jl
+++ examples/plots.jl
@@ -51,6 +51,15 @@ function plot_control()
     nothing
 end
 
+function plot_control_II()
+    log = load_log(basename(KiteViewers.plot_file[]); path=fulldir(KiteViewers.plot_file[]))
+    sl  = log.syslog
+    display(plotx(log.syslog.time, rad2deg.(sl.elevation), rad2deg.(sl.azimuth), -rad2deg.(wrap2pi.(sl.heading)), 100*sl.steering, rad2deg.(sl.var_07), rad2deg.(sl.course.-pi), rad2deg.(sl.var_09), rad2deg.(sl.var_09), sl.sys_state;
+            ylabels=["elevation [°]", "azimuth [°]", "psi [°]", "steering [%]", "chi_set", "chi", "psi_dot_set", "psi_dot", "fpp_state"],
+            fig="fpc", ysize=10))
+    nothing
+end
+
 function plot_winch_control()
     log = load_log(basename(KiteViewers.plot_file[]); path=fulldir(KiteViewers.plot_file[]))
     sl  = log.syslog
