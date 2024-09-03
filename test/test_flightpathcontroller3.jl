2a5714b20ded12d9ee1da457bb956c6c13b83b6d
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-09-03 18:22:13 +0200

    better fix

--- test/test_flightpathcontroller3.jl
+++ test/test_flightpathcontroller3.jl
@@ -93,7 +93,7 @@ end
 function test_parking(suppress_overshoot_factor = 3.0)
     global LAST_RES
     clear!(kps4)
-    # init_kcu(kcu, set)
+    KitePodModels.init_kcu!(kcu, set)
     integrator = KiteModels.init_sim!(kps4, stiffness_factor=0.04)
     simulate(integrator)
 end
