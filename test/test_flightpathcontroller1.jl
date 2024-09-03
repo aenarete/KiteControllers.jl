b4759cb533ae024b9925be2f17d6cbce4179ce5d
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-09-03 16:42:38 +0200

    fix test

--- test/test_flightpathcontroller1.jl
+++ test/test_flightpathcontroller1.jl
@@ -3,17 +3,21 @@ using Pkg
 if ! ("ControlPlots" ∈ keys(Pkg.project().dependencies))
     using TestEnv; TestEnv.activate()
 end
-using KiteControllers, Timers, ControlPlots; tic()
+using KiteControllers, KiteUtils, Timers, ControlPlots; tic()
 
-fcs = FPCSettings()
-fcs.dt = 0.02
+
+set = deepcopy(load_settings("system.yaml"))
+
+fcs = FPCSettings(dt=1/set.sample_freq)
 DURATION = 100.0
 SAMPLES = Int(DURATION / fcs.dt + 1)
 TIME = range(0.0, DURATION, SAMPLES)
 
 # Test the flight path controller against the simplified kite model as shown
 # in diagram docs/flight_path_controller_test1.png .
-fpc = FlightPathController(fcs)
+u_d0 = 0.01 * set.depower_offset
+u_d  = 0.01 * set.depower
+fpc = FlightPathController(fcs; u_d0, u_d)
 kite = KiteModel(fcs)
 kite.omega = 0.08
 v_a = 20.0
