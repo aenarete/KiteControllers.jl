63e4597224dc23a401fd85fc9e5b6b8171726eb6
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-10-01 17:15:38 +0200

    minor changes

--- examples/parking_wind_dir.jl
+++ examples/parking_wind_dir.jl
@@ -24,12 +24,13 @@ ssc::SystemStateControl = SystemStateControl(wcs, fcs, fpps; u_d0, u_d)
 dt::Float64 = wcs.dt
 
 # result of tuning
-fcs.p=0.60*20
-fcs.i=0.3
-fcs.d=12.34
+fcs.p=2.0 #1.5
+fcs.i=0.35
+fcs.d=13.25
+# fcs.use_chi = false
 
 # the following values can be changed to match your interest
-MAX_TIME::Float64 = 80
+MAX_TIME::Float64 = 100
 TIME_LAPSE_RATIO  =  4
 SHOW_KITE         = true
 # For position and velocity vectors of the model the ENU (East North Up) 
@@ -46,6 +47,48 @@ if ! @isdefined UPWIND_DIR_; const UPWIND_DIR_ = zeros(Int64(MAX_TIME/dt)); end
 if ! @isdefined HEADING; const HEADING = zeros(Int64(MAX_TIME/dt)); end
 if ! @isdefined STEERING; const SET_STEERING = zeros(Int64(MAX_TIME/dt)); end
 if ! @isdefined STEERING; const STEERING = zeros(Int64(MAX_TIME/dt)); end
+# """
+#     calc_heading_w(orientation, down_wind_direction = pi/2.0)
+
+# Calculate the heading vector in wind reference frame.
+# """
+# function calc_heading_w(orientation, down_wind_direction = pi/2.0)
+#     # create a unit heading vector in the xsense reference frame
+#     heading_sensor =  SVector(1, 0, 0)
+#     # rotate headingSensor to the Earth Xsens reference frame
+#     headingEX = fromKS2EX(heading_sensor, orientation)
+#     # rotate headingEX to earth groundstation reference frame
+#     headingEG = fromEX2EG(headingEX)
+#     # rotate headingEG to headingW and convert to 2d HeadingW vector
+#     fromEG2W(headingEG, down_wind_direction)
+# end
+
+# """
+#     calc_heading(orientation, elevation, azimuth; upwind_dir=-pi/2, respos=true)
+
+# Calculate the heading angle of the kite in radians. The heading is the direction
+# the nose of the kite is pointing to. 
+# If respos is true the heading angle is defined in the range of 0 .. 2π,
+# otherwise in the range -π .. π
+# """
+# function calc_heading(orientation, elevation, azimuth; upwind_dir=-pi/2, respos=true)
+#     down_wind_direction = wrap2pi(upwind_dir + π)
+#     headingSE = fromW2SE(calc_heading_w(orientation, down_wind_direction), elevation, azimuth)
+#     angle = atan(headingSE.y, headingSE.x) # - π
+#     if angle < 0 && respos
+#         angle += 2π
+#     end
+#     angle
+# end
+
+
+# #  function calc_heading(s::KPS4; upwind_dir=upwind_dir(s))
+# function calc_heading(s::KPS4; upwind_dir=-pi/2)
+#     orientation = orient_euler(s)
+#     elevation = calc_elevation(s)
+#     azimuth = calc_azimuth(s)
+#     KiteUtils.calc_heading(orientation, elevation, azimuth; upwind_dir)
+# end
 
 function simulate(integrator)
     upwind_dir=UPWIND_DIR
@@ -86,6 +129,10 @@ function simulate(integrator)
         sys_state.orient .= calc_orient_quat(kps4; old=true)
         T[i] = dt * i
         AZIMUTH[i] = sys_state.azimuth
+<<<<<<< HEAD
+=======
+        # HEADING[i] = wrap2pi(calc_heading(kps4)) 
+>>>>>>> 804070c (minor changes)
         HEADING[i] = wrap2pi(sys_state.heading)
         on_new_systate(ssc, sys_state)
         if mod(i, TIME_LAPSE_RATIO) == 0
