83bd6dafd63925bf8e72e8e1230bd5966942f962
Uwe Fechner <fechner@aenarete.eu>
2024-10-27 18:49:22 +0100

    add parking_controller.jl

---
+++ examples/parking_controller.jl
@@ -0,0 +1,40 @@
+# prototype of a parking controller
+# Components: PID controller, NDI block, and gain scheduler
+
+@with_kw mutable struct ParkingControllerSettings @deftype Float64
+    # turn rate controller settings
+    kp=1
+    ki=0
+    kd=0
+    n = 10
+    # NDI block settings
+    va_min = 5.0   # minimum apparent wind speed
+    va_max = 100.0 # maximum apparent wind speed
+    k_ds = 2.0 # influence of the depower settings on the steering sensitivity
+    c1 = 0.048 # v9 kite model
+    c2 = 5.5   
+end
+
+"""
+    linearize(pcs::ParkingControllerSettings, psi_dot, psi, elevation, v_app; ud_prime=0)
+
+Nonlinear, dynamic inversion block (NDI) according to Eq. 6.4 and Eq. 6.12.
+
+Parameters:
+- psi_dot: desired turn rate in radians per second
+- psi: heading in radians
+- elevation: elevation angle in radians
+- v_app: apparent wind speed in m/s
+- ud_prime: depower setting in the range of 0 to 1, 0 means fully powered, 1 means fully depowered
+"""
+function linearize(pcs::ParkingControllerSettings, psi_dot, psi, elevation, v_app; ud_prime=0)
+    # Eq. 6.13: calculate va_hat
+    va_hat = clamp(v_app, pcs.va_min, pcs.va_max)
+    # Eq. 6.12: calculate the steering from the desired turn rate
+    u_s = (1.0 + pcs.k_ds * pcs.ud_prime) / (pcs.c1 * va_hat) * (psi_dot - pcs.c2 / va_hat * sin(psi) * cos(elevation))
+    if abs(psi_dot) < 1e-6
+        psi_dot = 1e-6
+    end
+    ndi_gain = clamp(u_s / psi_dot, -20.0, 20.0)
+    return u_s, ndi_gain
+end
