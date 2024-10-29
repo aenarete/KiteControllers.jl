16b79c07af2b75fb547d079bbbc4d0ac967307ea
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-10-29 11:53:54 +0100

    refactoring

--- examples/parking_controller.jl
+++ examples/parking_controller.jl
@@ -1,6 +1,6 @@
 # Prototype of a parking controller.
 # Components: PID controller, NDI block, and great circle navigation
-using DiscretePIDs, Parameters, Test, ControlPlots
+using DiscretePIDs, Parameters, Test
 import KiteControllers: calc_steering, wrap2pi, navigate
 
 @with_kw mutable struct ParkingControllerSettings @deftype Float64
@@ -17,7 +17,6 @@ import KiteControllers: calc_steering, wrap2pi, navigate
     k_ds = 2.0 # influence of the depower settings on the steering sensitivity
     c1 = 0.048 # v9 kite model
     c2 = 0     # a value other than zero creates more problems than it solves
-    last_ndi_gain = 0.0
 end
 
 mutable struct ParkingController
@@ -26,13 +25,14 @@ mutable struct ParkingController
     pid_outer::DiscretePID
     last_heading::Float64
     chi_set::Float64
+    last_ndi_gain::Float64
 end
 
 function ParkingController(pcs::ParkingControllerSettings; last_heading = 0.0)
     Ts = pcs.dt
     pid_tr    = DiscretePID(;K=pcs.kp_tr, Ti=pcs.kp_tr/ pcs.ki_tr, Ts)
     pid_outer = DiscretePID(;K=pcs.kp,    Ti=pcs.kp/ pcs.ki,       Ts)
-    return ParkingController(pcs, pid_tr, pid_outer, last_heading, 0)
+    return ParkingController(pcs, pid_tr, pid_outer, last_heading, 0, 0)
 end
 
 """
@@ -47,17 +47,18 @@ Parameters:
 - v_app: apparent wind speed in m/s
 - ud_prime: depower setting in the range of 0 to 1, 0 means fully powered, 1 means fully depowered
 """
-function linearize(pcs::ParkingControllerSettings, psi_dot, psi, elevation, v_app; ud_prime=0)
+function linearize(pc::ParkingController, psi_dot, psi, elevation, v_app; ud_prime=0)
+    pcs = pc.pcs
     # Eq. 6.13: calculate va_hat
     va_hat = clamp(v_app, pcs.va_min, pcs.va_max)
     # Eq. 6.12: calculate the steering from the desired turn rate
     u_s = (1.0 + pcs.k_ds * ud_prime) / (pcs.c1 * va_hat) * (psi_dot - pcs.c2 / va_hat * sin(psi) * cos(elevation))
     if abs(psi_dot) < 1e-6
-        ndi_gain = pcs.last_ndi_gain
+        ndi_gain = pc.last_ndi_gain
     else
         ndi_gain = clamp(u_s / psi_dot, -20, 20.0)
     end
-    pcs.last_ndi_gain = ndi_gain
+    pc.last_ndi_gain = ndi_gain
     return u_s, ndi_gain
 end
 
@@ -105,7 +106,7 @@ function calc_steering(pc::ParkingController, heading, chi_set; elevation=0.0, v
     pc.last_heading = heading
     psi_dot_in = pc.pid_tr(psi_dot_set, psi_dot)
     # linearize the NDI block
-    u_s, ndi_gain = linearize(pc.pcs, psi_dot_in, heading, elevation, v_app; ud_prime)
+    u_s, ndi_gain = linearize(pc, psi_dot_in, heading, elevation, v_app; ud_prime)
     u_s, ndi_gain, psi_dot, psi_dot_set
 end
 
@@ -128,7 +129,7 @@ function test_linearize()
         # set the depower setting
         ud_prime = 0.5
         # linearize the NDI block
-        u_s, ndi_gain = linearize(pc.pcs, psi_dot, psi, elevation, v_app; ud_prime)
+        u_s, ndi_gain = linearize(pc, psi_dot, psi, elevation, v_app; ud_prime)
         @test u_s ≈ 0.41666666666666674
         @test ndi_gain ≈ 4.166666666666667
     end
