c0cf54b8c7c15311adeeee12ef84f94ac4942ba7
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-04-22 15:09:28 +0200

    Fix test

--- test/aqua.jl
+++ test/aqua.jl
@@ -2,7 +2,7 @@ using Aqua
 @testset "Aqua.jl" begin
     Aqua.test_all(
       KiteControllers;
-      stale_deps=(ignore=[:PyCall, :REPL],), # CodecXz is used during precompilation only
+      stale_deps=(ignore=[:PyCall, :REPL, :Timers],), # CodecXz is used during precompilation only
       deps_compat=(ignore=[:PyCall],),                 # PyCall is needed for CI to recompile Python
       #piracies=false                                   # the norm function is doing piracy for performance reasons
     )
