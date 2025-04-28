ad98e512e4f6600d813962c986be243377f65a69
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-04-28 22:17:19 +0200

    Add steering to aerodynamics plot

--- examples/plots.jl
+++ examples/plots.jl
@@ -214,8 +214,8 @@ function plot_aerodynamics(plot_lift_drag = false)
                       fig="CD_tot as function of AoA"))
 
     else
-        display(plotx(sl.time, sl.var_08, rad2deg.(sl.AoA), rad2deg.(sl.var_16); 
-                    ylabels=["LoD [-]", L"AoA~[°]", L"side\_slip~[°]"],
+        display(plotx(sl.time, sl.var_08, rad2deg.(sl.AoA), 100*sl.steering, rad2deg.(sl.var_16); 
+                    ylabels=["LoD [-]", L"AoA~[°]", "steering [%]", L"side\_slip~[°]"],
                     fig="aerodynamics"))
     end
     nothing
