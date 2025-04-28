416f37bfe84f214e48b84411f3eeb3850edb0d58
Uwe Fechner <u.fechner-1@tudelft.nl>
2025-04-28 22:17:05 +0200

    Bugfix

--- test/create_sys_image.jl
+++ test/create_sys_image.jl
@@ -6,14 +6,14 @@ if ! ("ControlPlots" ∈ keys(Pkg.project().dependencies))
 end
 
 @info "Loading packages ..."
-using BenchmarkTools, KiteUtils, NLsolve, Parameters, StaticArrays, StaticArrayInterface, WinchModels, PackageCompiler, KiteViewers, 
+using BenchmarkTools, KiteUtils, NLsolve, Parameters, StaticArrays, WinchModels, PackageCompiler, KiteViewers, 
       KiteModels, KitePodModels, StructTypes, YAML, StatsBase, ControlPlots
 
 @info "Creating sysimage ..."
 push!(LOAD_PATH,joinpath(pwd(),"src"))
 
 PackageCompiler.create_sysimage(
-    [:BenchmarkTools, :KiteUtils, :NLsolve, :Parameters, :StaticArrays, :StaticArrayInterface, :WinchModels, :KiteViewers, 
+    [:BenchmarkTools, :KiteUtils, :NLsolve, :Parameters, :StaticArrays, :WinchModels, :KiteViewers, 
      :KiteModels, :KitePodModels, :StructTypes, :YAML, :StatsBase, :ControlPlots];
     sysimage_path="kps-image_tmp.so",
     include_transitive_dependencies=true,
