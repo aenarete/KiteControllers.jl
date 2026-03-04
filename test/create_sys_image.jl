0e5c955b841da5a5b2cd1887c21953f57a56cf17
Uwe Fechner <u.fechner-1@tudelft.nl>
2026-03-04 17:00:45 +0100

    Next try

--- test/create_sys_image.jl
+++ test/create_sys_image.jl
@@ -1,15 +1,19 @@
 using Pkg
+if ! ("ControlPlots" ∈ keys(Pkg.project().dependencies))
+    Pkg.activate(@__DIR__)
+end
 
 @info "Loading packages ..."
-using BenchmarkTools, KiteUtils, NLsolve, Parameters, StaticArrays, WinchModels, PackageCompiler, KiteViewers, 
-      KiteModels, KitePodModels, StructTypes, YAML, StatsBase, ControlPlots
+using BenchmarkTools, ControlPlots, KiteModels, KitePodModels, KiteUtils, KiteViewers,
+      NLsolve, PackageCompiler, Parameters, StaticArrays, Statistics, StructTypes,
+      WinchModels, YAML
 
 @info "Creating sysimage ..."
 push!(LOAD_PATH,joinpath(pwd(),"src"))
 
 PackageCompiler.create_sysimage(
     [:BenchmarkTools, :KiteUtils, :NLsolve, :Parameters, :StaticArrays, :WinchModels, :KiteViewers, 
-     :KiteModels, :KitePodModels, :StructTypes, :YAML, :StatsBase, :ControlPlots];
+     :KiteModels, :KitePodModels, :StructTypes, :YAML, :Statistics, :ControlPlots];
     sysimage_path="kps-image_tmp.so",
     include_transitive_dependencies=true,
     precompile_execution_file=joinpath("test", "test_for_precompile.jl")
