23ce54581c5573c916421980ee73672a3a2264ec
Uwe Fechner <u.fechner-1@tudelft.nl>
2026-03-06 04:53:35 +0100

    Cleanup

--- test/precompile_batch_plot.jl
+++ test/precompile_batch_plot.jl
@@ -13,15 +13,6 @@ let
         end
     end
 
-    # Also exercise plot_timing / plot_timing2 (not in MENU_ITEMS)
-    for (name, fn) in [("plot_timing", plot_timing), ("plot_timing2", plot_timing2)]
-        println("Precompiling $name ...")
-        try
-            fn()
-        catch e
-            println("  Skipped $name: $e")
-        end
-    end
 end
 
 @info "Precompile script precompile_batch_plot.jl has completed execution."
