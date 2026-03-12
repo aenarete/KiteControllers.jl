80abbd8c078f22a0bdcf85894b916ac92dd65db6
Uwe Fechner <u.fechner-1@tudelft.nl>
2026-03-12 13:11:55 +0100

    Fix warning

--- examples/tune_1p.jl
+++ examples/tune_1p.jl
@@ -10,7 +10,7 @@ set = deepcopy(load_settings("system.yaml"))
 set.abs_tol = 0.00006
 set.rel_tol = 0.0001
 
-using KiteControllers, KiteModels, NOMAD, ControlPlots
+using ControlPlots, KiteControllers, KiteModels, NOMAD
 using KiteControllers: calc_steering
 
 kcu::KCU = KCU(set)
