682d2f56a09598779db8f2b1cbcf0a7bc6ec6a03
Uwe Fechner <fechner@aenarete.eu>
2026-03-29 14:26:21 +0200

    Try to reduce memory usage

--- test/create_sys_image.jl
+++ test/create_sys_image.jl
@@ -11,14 +11,29 @@ using BenchmarkTools, ControlPlots, KiteModels, KitePodModels, KiteUtils, KiteVi
 @info "Creating sysimage ..."
 push!(LOAD_PATH,joinpath(pwd(),"src"))
 
+GC.gc(true)
+let mem = Sys.free_memory() / 1024^2
+    @info "Free memory: $(round(mem; digits=1)) MB"
+    if haskey(ENV, "JULIA_IMAGE_THREADS")
+        @info "JULIA_IMAGE_THREADS: $(ENV["JULIA_IMAGE_THREADS"])"
+    else
+        @info "JULIA_IMAGE_THREADS not defined!"
+    end
+end
+
 precompile_script = joinpath("test", "precompile_all.jl")
 open(precompile_script, "w") do io
     println(io, """include("test_for_precompile.jl")""")
     println(io, """include("precompile_batch_plot.jl")""")
 end
 
+GC.gc(true)
+let mem = Sys.free_memory() / 1024^2
+    @info "Free memory: $(round(mem; digits=1)) MB"
+end
+
 PackageCompiler.create_sysimage(
-    [:BenchmarkTools, :KiteUtils, :NLsolve, :Parameters, :StaticArrays, :WinchModels, :WinchControllers, :KiteViewers, 
+    [:KiteUtils, :NLsolve, :Parameters, :StaticArrays, :WinchModels, :WinchControllers, :KiteViewers, 
      :KiteModels, :KitePodModels, :StructTypes, :YAML, :Statistics, :ControlPlots];
     sysimage_path="kps-image_tmp.so",
     include_transitive_dependencies=true,
