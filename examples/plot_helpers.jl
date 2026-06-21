5e2c84b91738f76cb7cf2f725a49a7fc269ab12e
Uwe Fechner <u.fechner-1@tudelft.nl>
2026-06-21 17:08:06 +0200

    Improve performance

--- examples/plot_helpers.jl
+++ examples/plot_helpers.jl
@@ -1,13 +1,13 @@
 # Shared helper accessors used by both plots.jl and batch_plot.jl
 
 function l_tether(sl)
-    hcat(sl.l_tether...)[1,:]
+    getindex.(sl.l_tether, 1)
 end
 
 function force(sl)
-    hcat(sl.winch_force...)[1,:]
+    getindex.(sl.winch_force, 1)
 end
 
 function v_reelout(sl)
-    hcat(sl.v_reelout...)[1,:]
+    getindex.(sl.v_reelout, 1)
 end
